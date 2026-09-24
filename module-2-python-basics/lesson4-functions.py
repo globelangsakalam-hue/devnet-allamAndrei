97646c377620bb147bd140e919f40661063b1441
70x7 <globelangsakalam@gmail.com>
2026-09-24 14:23:34 +0800

    function example
    function example, added name and section and date today

--- module-2-python-basics/lesson4-functions.py
+++ module-2-python-basics/lesson4-functions.py
@@ -1,1 +1,14 @@
+Allam, Karl Andrei T. 
+BSIT 3A
+9/24/2026
+
+
+A Python Function is a block of Code whch only run when it's called
+for example:
+
+def akong_function():                  > this is a function name, contains letters and underscore, it is case sensitive..
+  print("hello, from function")        
+
+akong_function()                       
+note!!!                                > you can use function to avoid repetitions
 
